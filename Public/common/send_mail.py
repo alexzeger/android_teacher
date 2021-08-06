c4e11c8aa67306111ca2831a18af4363831af939
alex <2540911284@qq.com>
2021-08-06 15:08:43 +0800

    1.0.0.4
    添加sendemail

--- Public/common/send_mail.py
+++ Public/common/send_mail.py
@@ -1,10 +1,10 @@
 import os
-import time 
-
+import time
 from email.mime.text import MIMEText
 from email.header import Header
 from smtplib import SMTP_SSL
 from email.mime.multipart import MIMEMultipart
+from email.header import Header
 from Config import globalconfig
 #测试报告的路径
 report_path = globalconfig.Report_path
@@ -23,21 +23,25 @@ def sendReport(file_new):
     sender_qq_mail = '2540911284@qq.com'
     #收件人的邮箱地址
     receiver = '2540911284@qq.com'
+    # 可添加多个收件人的邮箱
+    receives = ['2540911284@qq.com','baojiong@box-fairy.com']
     #邮件标题
-    mail_tilte ='自动化测试报告'
+    mail_tilte ='android_teacher自动化测试报告'
 
     msg = MIMEMultipart()
     msg["Subject"]=Header(mail_tilte,'utf-8')
     msg["From"] =sender_qq_mail
+    msg['To'] = ','.join(receives)
 
     #邮件正文内容
-    msg.attach(MIMEText('测试报告','html','utf-8'))
+    text_plain = MIMEText("附件是最新的android_teacher自动化测试报告，请查看", 'html', 'utf-8')
+    msg.attach(text_plain)
 
+    # 构造附件
     filename = "report.html"
     att3 =MIMEText(open(file_new,'rb').read(),'base64','utf-8')
     att3["Content-Type"] = 'application/octet-stream'
     # 这里的filename可以任意写，写什么名字，邮件中显示什么名字
-
     att3.add_header('Content-Disposition', 'attachment', filename=filename)
     msg.attach(att3)
 
@@ -47,15 +51,20 @@ def sendReport(file_new):
     # msg_root.attach(html)
 
     #ssl登录
-    smtp=SMTP_SSL(host_server)
-
+    # 邮箱设置时勾选了SSL加密连接，进行防垃圾邮件，SSL协议端口号要使用465
+    smtp=SMTP_SSL(host_server, 465)
+    # 向服务器标识用户身份
+    smtp.helo(host_server)
+    # 向服务器返回确认结果
     smtp.ehlo(host_server)
+    # 登录邮箱的账号和授权密码
     smtp.login(sender_qq,pwd)
-    smtp.sendmail(sender_qq_mail,receiver,msg.as_string())
+    print("开始发送邮件...")
+    # 开始进行邮件的发送，msg表示已定义的字典
+    smtp.sendmail(sender_qq_mail,receives,msg.as_string())
 
     smtp.quit()
-    print("Test Report has send out!!")
-
+    print("已发送邮件")
 
 # ####################查找测试报告，找出最新的测试报告###########################
 def newReport(testReport):
